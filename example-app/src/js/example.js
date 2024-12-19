31fbd0647be24c97950925ce3cb07a1828fb99fc
J Caso <jcaso@protonmail.com>
2024-12-19 16:45:02 +0100

    feat: add example react app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { NearbyConnections } from 'nearby-connections';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    NearbyConnections.echo({ value: inputValue })
-}
